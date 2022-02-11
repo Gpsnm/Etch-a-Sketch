311bf598893c0fc500894cf6f40fd9bb9fb67281
Gpsnm <adam-willetts@hotmail.co.uk>
2022-02-11 22:58:56 +0000

    updated version working on adding ability to change grid size

--- javascript.js
+++ javascript.js
@@ -2,12 +2,33 @@
 
 const button = document.querySelector('button');
 const container = document.getElementById('container');
+const reloadButton = document.querySelector('#reload')
+let gridSize = prompt('What Size Grid Would You Like?');
+newGridSize();
+
+
+// create new grid 
+
+function newGridSize(){
+    container.innerHTML = '';
+    // container.style.setProperty('grid-template-columns', gridSize,'2fr');
+    // container.style.setProperty('grid-template-rows',gridSize,'2fr');
+    for (i=0; i< gridSize; i++){
+        const  grid = document.createElement('div')
+grid.classList.add('grid');
+container.appendChild(grid);
+grid.onmouseover = function(){
+    this.style.backgroundColor = 'black'
+    }
+}};
 
 
 // reload button 
-const reloadButton = document.querySelector('#reload')
+
 reloadButton.addEventListener('click' , () => {
     location.reload()
+    newGridSize()
+    
 });
     
 // create grid function 
